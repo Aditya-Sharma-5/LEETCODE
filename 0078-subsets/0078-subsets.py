69ea2ab59f580c86dd61ad3124689c690d54b0ff
Aditya Sharma <138656480+Aditya-Sharma-5@users.noreply.github.com>
2023-08-18 01:17:54 +0530

    Time: 35 ms (95.01%), Space: 16.6 MB (10.94%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def solve(i, curr):
-            if i >= len(nums):
-                ans.append(curr.copy())
-                return 
-            curr.append(nums[i])
-            solve(i+1, curr)
-
-            curr.pop()
-            solve (i+1, curr)
-        solve(0,[])
+        def solve(nums,ans,temp,index):
+            
+            if(index >= len(nums)):
+                ans.append(temp.copy())
+                return
+            
+            # lunga
+            temp.append(nums[index])
+            solve(nums,ans, temp ,index+1)
+            temp.pop()
+            
+            # nhilunga
+            solve(nums,ans, temp,index+1)
+            
+        solve(nums,ans,[],0)
+        
         return ans
