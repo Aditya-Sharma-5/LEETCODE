6bc5019416b74a71fd44899a7fda74e493bd395c
Aditya Sharma <138656480+Aditya-Sharma-5@users.noreply.github.com>
2024-07-06 23:37:26 +0530

    Time: 8 ms (64.14%), Space: 8.6 MB (62.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
-        if(s==t){
-            return true;
-        }
-        else{
+       // Brute Force Approach
+        // sort(s.begin() , s.end());
+        // sort(t.begin() , t.end());
+        // if(s==t){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+
+        // Optimized 
+        if (s.length() != t.length()) {
             return false;
         }
+        
+        vector<int> freq(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        
+        for (int i = 0; i < freq.size(); i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
