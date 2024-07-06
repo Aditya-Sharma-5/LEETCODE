eb27432303202a532bf453f281629d584865a2b1
Aditya Sharma <138656480+Aditya-Sharma-5@users.noreply.github.com>
2024-07-06 22:10:14 +0530

    Time: 0 ms (100%), Space: 10.1 MB (75.97%) - LeetHub

---
+++ 0926-find-and-replace-pattern/0926-find-and-replace-pattern.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    void createMapping(string &str){
+        char start = 'a';
+        char map[300] = {0};
+
+        for(auto ch:str){
+            if(map[ch] == 0){
+                map[ch] = start++;
+            }
+        }
+
+        for(int i = 0; i<str.length() ; i++){
+            char ch = str[i];
+            str[i] = map[ch];
+        }
+    }
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        //firstly normalise the pattern 
+        createMapping(pattern);
+
+        vector<string> ans;
+        for(string s: words)
+        {
+            string temp = s;
+            createMapping(temp);
+            if(temp == pattern){
+                ans.push_back(s);
+            }
+        }
+        return ans;
+    }
+};
