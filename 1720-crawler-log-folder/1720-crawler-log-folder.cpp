892bf225a53fc95f1b2f58832fe77fc605ba2307
Aditya Sharma <138656480+Aditya-Sharma-5@users.noreply.github.com>
2024-07-10 21:29:39 +0530

    Time: 0 ms (100%), Space: 13.8 MB (84.88%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        vector<string> ans ; 
-        for(auto log : logs){
-            if(log == "../"){
-                if(!ans.empty()){
-                    ans.pop_back();
-                }
-            }
-            else if( log != "./"){
-                ans.push_back(log);
+        // vector<string> ans ; 
+        // for(auto log : logs){
+        //     if(log == "../"){
+        //         if(!ans.empty()){
+        //             ans.pop_back();
+        //         }
+        //     }
+        //     else if( log != "./"){
+        //         ans.push_back(log);
+        //     }
+        // }
+        // return ans.size();
+
+        int n = 0;
+        for (int i = 0; i < logs.size(); i++) {
+            if (logs[i] == "../") {
+               if(n>0) n--;
+            } else if (logs[i] == "./") {
+                continue;
+            } else {
+                n++;
             }
         }
-        return ans.size();
+        if(n<0) return 0;
+        return n;
     }
 };
